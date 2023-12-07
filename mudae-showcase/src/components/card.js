1dbaed00f3c9b7286b733f2fd4e3eed4105d0f58
Scott Dolphin <nathanjj1225@gmail.com>
2023-12-07 12:20:42 -0800

    fixme: cannot find module | line 22

--- mudae-showcase/src/components/card.js
+++ mudae-showcase/src/components/card.js
@@ -1,21 +1,38 @@
 
 // import weapons from './data/genshinData.json'
 
-// const myWeapons = JSON.parse(weapons);
 
 export default function Card({ characterName, weapon, level, constellation, ascension }) {
+
     // console.log(myWeapons);
     return (
         <div className="Card">
-            {/* image here maybe using map with characterName:image? */}
+            <Pfp cName={characterName.replace(/([a-z])([A-Z])/g, '$1_$2')} />
+            <img src={require('./data/characterImg/Aether_Icon.png')}></img>
             <h2>{characterName.replace(/([a-z])([A-Z])/g, '$1 $2')}</h2>
-            <p>Equipped: {weapon.replace(/([a-z])([A-Z])/g, '$1 $2')}</p>
+            <h3>Equipped: {weapon.replace(/([a-z])([A-Z])/g, '$1 $2')}</h3>
             <p>Level {level}</p>
             <p>C{constellation}</p>
             <p>A{ascension}</p>
         </div>
     )
 }
+
+function Pfp({ cName }) {
+    let imgLink = "./data/characterImg/" + cName + "_Icon.png";
+    return (
+        <>
+            <img src={require(imgLink)} alt={cName}></img>
+            <p>{imgLink}</p>
+        </>
+
+    )
+}
+
+// function stringConvert( cName) {
+//     let convertedString = "./data/characterImg/" + cName + "_Icon.png";
+//     return convertedString;
+// }
 
 
 
