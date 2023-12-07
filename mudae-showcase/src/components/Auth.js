0a12b79bdb7c6d353836ee0a3ad964b93f08cade
Louis Oporto <louisoporto@yahoo.com>
2023-12-06 17:58:15 -0800

    Functional Log In Page

--- mudae-showcase/src/components/Auth.js
+++ mudae-showcase/src/components/Auth.js
@@ -1,16 +1,40 @@
-import pb from './lib/pocketbase';
+import pb from './pocketbase';
+import { useForm } from 'react-hook-form';
+import { useState } from 'react';
+import Navbar from './navbar';
 
-export default function App() {
+export default function Auth() {
+  const { register, handleSubmit } = useForm();
+  const [loading, isLoading] = useState(pb.authStore.isValid);
+
+  async function Authenticate(data) {
+    const authData = await pb.collection("users").authWithPassword(data.email, data.password);
+    console.log(authData);
+    isLoading(pb.authStore.isValid);
+  }
+
+  if (!pb.authStore.isValid) {
     return (
-        <>
-            <h1>Logged In: {pb.authStore.isValid.toString()}</h1>
+      <>
+        <div className="App">
+          <h1>Log In:</h1>
 
-            <form>
-                <input type="text" placeholder="email" />
-                <input type="password" placeholder="password" />
+          <form onSubmit={handleSubmit(Authenticate)}>
+            <input type="text" placeholder="email" {...register("email")} />
+            <input type="password" placeholder="password" {...register("password")} />
 
-                <button type="submit">Login</button>
-            </form>
-        </>
+            <button type="submit">Login</button>
+          </form>
+        </div>
+      </>
     );
+  } else {
+    return (
+      <>
+        <div className="App">
+          <Navbar/ >
+        </div>
+      </>
+    );
+  }
 }
