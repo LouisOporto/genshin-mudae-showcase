71b7220e22bf00a5d75a09e78843543feb242e21
Louis Oporto <louisoporto@yahoo.com>
2023-12-08 00:26:23 -0800

    Added creating accounts

--- mudae-showcase/src/components/Auth.js
+++ mudae-showcase/src/components/Auth.js
@@ -1,28 +1,55 @@
 import pb from './pocketbase';
 import { useForm } from 'react-hook-form';
-//import Navbar from './navbar';
+import { useState } from 'react';
 
 export default function Auth() {
   const { register, handleSubmit } = useForm();
-  
-  async function Authenticate(data) {
+  const [mode, setMode] = useState("Log In:");
+  const [changer, setChanger] = useState("Create an account?")
+  const [control, setControl] = useState("Login");
 
-    const authData = await pb.collection("users").authWithPassword(data.email, data.password);
-    console.log(authData);
-    window.location.reload(false);
+  function handleAuth() {
+    if (mode == "Log In:") {
+      setMode("Sign Up");
+      setChanger("Nevermind");
+      setControl("Signup");
+    } else {
+      setMode("Log In:");
+      setChanger("Create an account?");
+      setControl("Login");
+    }
+  }
+
+  async function Authenticate(data) {
+    if (mode == "Log In:") {
+      const authData = await pb.collection("users").authWithPassword(data.email, data.password);
+      console.log(authData);
+      window.location.reload(false);
+    } else {
+      const newAcc = await pb.collection("users").create({
+        email: `${data.email}`,
+        password: `${data.password}`,
+        passwordConfirm: `${data.password}`,
+      });
+      const authData = await pb.collection("users").authWithPassword(data.email, data.password);
+      console.log(authData);
+      window.location.reload(false);
+    }
   }
 
   return (
     <>
       <div className="App">
-        <h1>Log In:</h1>
+        <h1>{mode}</h1>
 
         <form onSubmit={handleSubmit(Authenticate)}>
           <input type="text" placeholder="email" {...register("email")} />
           <input type="password" placeholder="password" {...register("password")} />
 
-          <button type="submit">Login</button>
+          <button type="submit">{control}</button>
         </form>
+        <button id="createAccount" onClick={() => handleAuth()}>{changer}</button>
+
       </div>
     </>
   );
